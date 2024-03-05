c41fb420c4d41a257a09c4b304b079f577f828b7
Rauljp16 <raul_astra16@hotmail.com>
2024-03-05 19:56:44 +0100

    estructura de la web 5

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,16 @@
 import catrina from '../img/catrina.png';
 import taco from '../img/taco.jpg';
+import comida1 from '../img/comida1.jpg';
+import comida2 from '../img/comida2.jpg';
+import comida3 from '../img/comida3.jpg';
 
 function Body() {
   return (
     <div className="  flex flex-col items-center gap-6 h-screen w-full ">
       <img src={catrina} className="w-1/2" />
+      <img src={comida1} className="w-1/2" />
+      <img src={comida2} className="w-1/2" />
+      <img src={comida3} className="w-1/2" />
       <div className="   flex bottom-24 p-4 w-1/2 h-auto backdrop-blur-sm bg-white/30 rounded-md text-white text-3xl  items-center leading-relaxed">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque deleniti
         officia consequuntur blanditiis ea. Repellat, quasi rem eos atque sunt
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,27 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className=" fixed bottom-0 left-0 flex items-center  w-full h-16 ">
-      <div className=" mx-auto rounded-2xl flex justify-around items-center text-white w-2/4 h-2/3 cursor-pointer backdrop-blur-sm bg-white/30">
-        <Link to={'/'}>Inicio</Link>
+    <div className=" fixed bottom-5 left-0 flex items-center  w-full h-20 ">
+      <div className=" mx-auto rounded-2xl flex justify-around items-center w-2/4 h-2/3 cursor-pointer backdrop-blur-md bg-white/40 text-fuchsia-500 text-lg ">
+        <Link
+          to={'/'}
+          className=" px-4 hover:text-2xl   hover:text-cyan-600 hover:border-2 border-cyan-500 hover:rounded-2xl"
+        >
+          Inicio
+        </Link>
         Reservas
-        <Link to={'/Carta'}>Carta</Link>
-        <Link to={'/contacto'}>Contacto</Link>
+        <Link
+          to={'/Carta'}
+          className="px-4 hover:text-2xl   hover:text-cyan-600 hover:border-2 border-cyan-500 hover:rounded-2xl"
+        >
+          Carta
+        </Link>
+        <Link
+          to={'/contacto'}
+          className="px-4 hover:text-2xl   hover:text-cyan-600 hover:border-2 border-cyan-500 hover:rounded-2xl"
+        >
+          Contacto
+        </Link>
       </div>
     </div>
   );
