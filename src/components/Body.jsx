2109f1ce2800d623232ee8e5c91a652304d9676b
Rauljp16 <raul_astra16@hotmail.com>
2024-03-12 00:22:27 +0100

    estructura de la web 5.1

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,3 @@
-import catrina from '../img/catrina.png';
 import taco from '../img/taco.jpg';
 import comida1 from '../img/comida1.jpg';
 import comida2 from '../img/comida2.jpg';
@@ -6,18 +5,17 @@ import comida3 from '../img/comida3.jpg';
 
 function Body() {
   return (
-    <div className="  flex flex-col items-center gap-6 h-screen w-full ">
-      <img src={catrina} className="w-1/2" />
-      <img src={comida1} className="w-1/2" />
-      <img src={comida2} className="w-1/2" />
-      <img src={comida3} className="w-1/2" />
-      <div className="   flex bottom-24 p-4 w-1/2 h-auto backdrop-blur-sm bg-white/30 rounded-md text-white text-3xl  items-center leading-relaxed">
+    <div className=" relative top-14 flex flex-col items-center gap-16 w-full">
+      <img src={comida1} />
+      <img src={comida2} />
+      <img src={comida3} />
+      <div className="   flex p-4 backdrop-blur-sm bg-white/30 rounded-md text-white text-3xl  items-center leading-relaxed">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque deleniti
         officia consequuntur blanditiis ea. Repellat, quasi rem eos atque sunt
         animi laboriosam suscipit natus voluptate praesentium? Quidem quisquam
         eligendi nostrum.
       </div>
-      <img src={taco} className=" w-96 h-96 rounded-md" />
+      <img src={taco} />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
     <div>
-      <div className="absolute left-0">
-        <div className=" fixed flex w-full items-center justify-center text-white h-20  ">
-          <p className=" text-5xl flex uppercase ">Restaurante mexico</p>
+      <div>
+        <div className=" sticky flex w-full items-center justify-center h-20 backdrop-blur-md bg-white/40 text-fuchsia-500 font-serif">
+          <p className=" text-6xl flex uppercase ">Restaurante mexico</p>
         </div>
       </div>
     </div>
