2164c5d28bd1c156ada2a56cdc83a2dec5374247
Rauljp16 <raul_astra16@hotmail.com>
2024-03-18 20:29:02 +0100

    menu responsive1

--- src/components/BurguerMenu.jsx
+++ src/components/BurguerMenu.jsx
@@ -5,27 +5,31 @@ const BurguerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="absolute  flex w-full flex-col items-end justify-center">
-      <div className="hidden sm:block">
-        <div className="mx-4 flex items-center text-lg">
-          <Link to="/" className="mx-4">
-            Inicio
-          </Link>
-          <Link to="/reservas" className="mx-4">
-            Reservas
-          </Link>
-          <Link to="/carta" className="mx-4">
-            Carta
-          </Link>
-          <Link to="/contacto" className="mx-4">
-            Contacto
-          </Link>
-        </div>
+    <div className=" relative top-0 h-full w-full ">
+      <div
+        className={`${
+          !isOpen
+            ? 'animate-slide-up fixed left-0 top-0 flex w-full  -translate-y-72 flex-col items-center gap-8 rounded-b-xl bg-orange-400 p-8 text-xl shadow-xl sm:hidden'
+            : ' animate-slide-down fixed left-0 top-0 flex w-full flex-col items-center gap-8 rounded-b-xl bg-orange-400 p-8 text-xl shadow-xl sm:flex'
+        }`}
+      >
+        <Link to="/" className="mx-4">
+          Inicio
+        </Link>
+        <Link to="/reservas" className="mx-4">
+          Reservas
+        </Link>
+        <Link to="/carta" className="mx-4">
+          Carta
+        </Link>
+        <Link to="/contacto" className="mx-4">
+          Contacto
+        </Link>
       </div>
-      <div className="block sm:hidden">
+      <div className=" flex items-center justify-end sm:hidden">
         <button onClick={() => setIsOpen(!isOpen)}>
           <svg
-            className="relative z-30 h-8 w-8"
+            className="absolute right-0 z-30 h-8 w-8 "
             fill="none"
             strokeLinecap="round"
             strokeLinejoin="round"
@@ -34,31 +38,19 @@ const BurguerMenu = () => {
             stroke="currentColor"
           >
             {isOpen ? (
-              <path d="M6 18L18 6M6 6l12 12"></path>
+              <path
+                d="M6 18L18 6M6 6l12 12"
+                className="origin-center -rotate-180 transition duration-1000"
+              ></path>
             ) : (
-              <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
+              <path
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                className="origin-center rotate-180 transition duration-1000"
+              ></path>
             )}
           </svg>
         </button>
       </div>
-      {isOpen && (
-        <div className="animate-slide-down  fixed left-0 right-0 top-0 w-full rounded-b-lg border-b-2 border-black/10 bg-orange-400 py-8  sm:hidden">
-          <div className="flex flex-col items-center text-lg ">
-            <Link to="/" className="px-4 py-4 text-lg  hover:text-white ">
-              Inicio
-            </Link>
-            <Link to="/reservas" className="px-4 py-4 hover:text-white">
-              Reservas
-            </Link>
-            <Link to="/carta" className="px-4 py-4 hover:text-white">
-              Carta
-            </Link>
-            <Link to="/contacto" className="px-4 py-4 hover:text-white">
-              Contacto
-            </Link>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
