e0e7cc8c43749b988d95c30e6b2d155354b288e8
Rauljp16 <raul_astra16@hotmail.com>
2024-03-21 17:15:38 +0100

    contacto

--- src/components/BurguerMenu.jsx
+++ src/components/BurguerMenu.jsx
@@ -23,8 +23,8 @@ const BurguerMenu = () => {
       <div
         className={`${
           isOpen
-            ? ' opacity-1 fixed left-0 top-0 z-30 flex w-full -translate-x-0 flex-col items-center gap-8 rounded-b-xl bg-orange-400 p-8 text-xl shadow-xl transition duration-700 sm:flex'
-            : 'disabled: pointer-events-none fixed left-0 top-0 flex w-full translate-x-44 flex-col items-center gap-8 rounded-b-xl bg-orange-400 p-8 text-xl opacity-0 shadow-xl transition duration-700 sm:flex'
+            ? ' opacity-1 fixed left-0 top-0 z-30 flex w-full  -translate-x-0 flex-col items-center gap-10 rounded-b-xl bg-orange-400 py-16 text-xl shadow-xl transition duration-700 sm:flex'
+            : 'disabled: pointer-events-none fixed left-0 top-0 flex w-full translate-x-44 flex-col items-center gap-10 rounded-b-xl bg-orange-400 py-16 text-xl opacity-0 shadow-xl transition duration-700 sm:flex'
         }`}
       >
         <Link
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,15 @@
 import Location from './Location';
+import decoration4 from '../img/decoration4.png';
 
 function Contact() {
   const apiKey = 'TU_API_KEY';
   const mapCenter = { lat: 37.97, lng: -1.22 };
   const mapZoom = 14;
   return (
-    <div className="mt-10 flex h-screen flex-col gap-10 border-2 border-black pt-10">
-      <section className="relative h-1/3 bg-orange-400">
-        <div className="h-full bg-white/30 text-2xl text-white backdrop-blur-sm">
+    <div className="relative  flex h-screen flex-col  ">
+      <img src={decoration4} className="absolute bottom-0 h-72 w-full" />
+      <section className="relative h-1/3">
+        <div className="flex h-full flex-col items-center justify-around bg-zinc-900 text-2xl text-white">
           <p>Mexico</p>
           <p>Direccion</p>
           <p>Correo electronico</p>
@@ -15,7 +17,7 @@ function Contact() {
           <div>redes sociales</div>
         </div>
       </section>
-      <section className="relative h-1/3 bg-zinc-900 p-4">
+      <section className=" h-1/2 ">
         <Location apiKey={apiKey} center={mapCenter} zoom={mapZoom} />
       </section>
     </div>
